e5b514d50c53bb0fd263611563d89d67331f1a0a
Anne Brown <anne@browndomain.com>
2024-04-11 01:45:24 -0400

    ref/intro

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -90,7 +90,7 @@ const config: Config = {
             },
             {
               label: 'Ref',
-              to: '/ref/intro'
+              to: '/ref'
             }
           ],
         },
