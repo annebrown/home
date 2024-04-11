fcefad2014f0734dd4cd2befcc533a7776fa77d0
Anne Brown <anne@browndomain.com>
2024-04-11 08:37:30 -0400

    what changes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -47,7 +47,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'browndomain.com',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
