aa53f31d6c3da3fe399b498be62139cfe4be5b62
Anne Brown <anne@browndomain.com>
2024-05-18 14:03:08 -0400

    404

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -111,7 +111,7 @@ const config: Config = {
           [
             {
               label: 'My Cheatsheets on W3S',
-              href: ' https://cheatsheets.w3spaces.com/',
+              href: 'https://cheatsheets.w3spaces.com/',
             },
            
           ],
